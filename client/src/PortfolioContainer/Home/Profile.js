c1b18a19885d5e1c08988ed3c281997ab265a9c5
manecci <pmanecci@gmail.com>
2022-03-02 21:29:33 -0500

    Home assets and styling complete

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Typical from 'react-typical';
+import './Profile.css';
 
 export default function Profile() {
     return (
@@ -6,6 +8,7 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
+                        <div className='colz-icon'>
                         <a href='https://www.linkedin.com/in/pmanecci/' target="_blank">
                             <i className='fa fa-linkedin-square'></i>
                         </a>
@@ -18,6 +21,51 @@ export default function Profile() {
                         <a href='https://www.instagram.com/pattymanecci/' target="_blank">
                             <i className='fa fa-instagram'></i>
                         </a>
+                        </div>                    
+                    </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {""}
+                            Hi, I'm Patty Manecci
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {""}
+                            <h1>
+                                {""}
+                                <Typical
+                                loop={Infinity}
+                                steps={[
+                                    "Software Engineer ",
+                                    1000,
+                                    "Jill of All Trades",
+                                    1000,
+                                    "Coffee Addict",
+                                    1000,
+                                    "Avid Traveler",
+                                    1000,
+                                ]}
+                                />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                Specializing in FrontEnd Web Design and Development
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-options'>
+                        <button className='btn primary-btn'>
+                            {""}
+                            Hire Me{" "}
+                        </button>
+                        <a href="resume.pdf" download="PattyManecci resume.pdf">
+                            <button className='btn highlighted-btn'>Get Resume</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'>
+
                     </div>
                 </div>
             </div>
