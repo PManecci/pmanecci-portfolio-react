d85e5795d0ae277e5ebb9a10d313bb24a1783f18
manecci <pmanecci@gmail.com>
2022-03-05 14:29:09 -0500

    fix .get route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/", contactRoute);
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
-    app.get("*", (req, res) => (
+    app.get("/", (req, res) => (
         res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
     ))
 }
