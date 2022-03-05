d17e6dfd98599534cf147289a724f6e80f4ca7a8
manecci <pmanecci@gmail.com>
2022-03-05 14:18:26 -0500

    deploy to heroku

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(cors())
 
 app.use("/", contactRoute);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => (
+        res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
+    ))
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listening to port 5000`));
 
