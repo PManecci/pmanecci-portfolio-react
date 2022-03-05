46dc75fa17a547d37732f054cb1221ee649547bc
manecci <pmanecci@gmail.com>
2022-03-05 16:28:38 -0500

    resume styling fixed

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -10,22 +10,21 @@ export default function Resume(props) {
         return (
         <div className='resume-heading'>
             <div className='resume-main-heading'>
-                <div className='heading-bullet'>
-                    <span>{props.heading ? props.heading : ''}</span>
-                    {props.fromDate && props.toDate ? (
-                        <div className='heading-date'>
-                            {props.fromDate + "_" + props.toDate}
-                        </div>
-                    ): (
-                        <div></div>
-                    )}
-                </div>
-                <div className='resume-sub-heading'>
-                    <span>{props.subHeading ? props.subHeading : ''}</span>
-                </div>
-                <div className='resume-heading-description'>
-                    <span>{props.description ? props.description : ''}</span>
-                </div>
+                <div className='heading-bullet'></div>
+                <span>{props.heading ? props.heading : ''}</span>
+                {props.fromDate && props.toDate ? (
+                    <div className='heading-date'>
+                        {props.fromDate + "_" + props.toDate}
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+            </div>
+            <div className='resume-sub-heading'>
+                <span>{props.subHeading ? props.subHeading : ''}</span>
+            </div>
+            <div className='resume-heading-description'>
+                <span>{props.description ? props.description : ''}</span>
             </div>
         </div>
     )};
