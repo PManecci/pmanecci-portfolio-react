5f2896627601e747ed6cfc9cbceb175199cfcefc
manecci <pmanecci@gmail.com>
2022-03-05 14:50:30 -0500

    update get route

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(cors())
 
 app.use("/", contactRoute);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"))
-    app.get("/", (req, res) => (
-        res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
-    ))
+if(process.env.NODE_ENV) {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    });
 }
 
 const port = process.env.PORT || 5000;
